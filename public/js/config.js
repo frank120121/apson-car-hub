c8f41cb0921afc08f29e6696c56ac5b4271b240e
Francisco <francisco.lopez4256@gmail.com>
2025-12-07 23:44:42 -0700

    feat: Update Firebase configuration and enhance car fetching logic in the application

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAdmuFZDU76WIFr1SAfJk-NqCA8GjLRPN0",
   authDomain: "apson-car-hub.firebaseapp.com",
@@ -18,4 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
